9ebbbb5a0c8b32de51b20498d372d8420e462f6b
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-27 01:08:46 +0200

    Disable Spring Boot SecurityAutoConfiguration and add our own

--- solution/expenses/src/main/java/com/engagetech/expenses/ExpensesApplication.java
+++ solution/expenses/src/main/java/com/engagetech/expenses/ExpensesApplication.java
@@ -2,8 +2,12 @@ package com.engagetech.expenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class
+}, scanBasePackages = "com.engagetech.expenses")
 public class ExpensesApplication {
 
 	public static void main(String[] args) {
---
+++ solution/expenses/src/main/java/com/engagetech/expenses/security/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package com.engagetech.expenses.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+/**
+ * Basic security configuration
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${spring.security.user.name}")
+    private String username;
+
+    @Value("${spring.security.user.password}")
+    private String password;
+
+    @Value("${spring.security.user.roles}")
+    private String role;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        auth
+                .inMemoryAuthentication()
+                .passwordEncoder(encoder)
+                .withUser(username)
+                .password(encoder.encode(password))
+                .roles(role);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/static/assets/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        // To enable generation of csrf token:
+        //  - remove .csrf().disable() call on http
+        //  - and uncomment the following line
+        //http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+}
