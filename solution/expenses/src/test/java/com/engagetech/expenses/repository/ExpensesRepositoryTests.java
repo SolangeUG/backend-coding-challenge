7cf87a2bc8d1972f38d225ff0c8f1d9804742e28
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-27 01:27:33 +0200

    Add repository unit tests

---
+++ solution/expenses/src/test/java/com/engagetech/expenses/repository/ExpensesRepositoryTests.java
@@ -0,0 +1,62 @@
+package com.engagetech.expenses.repository;
+
+import com.engagetech.expenses.model.Expense;
+import com.engagetech.expenses.model.ValueAddedTaxRate;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+/**
+ * Expenses Repository Unit Tests
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+public class ExpensesRepositoryTests {
+
+    @Autowired
+    private TestEntityManager manager;
+
+    @Autowired
+    private ExpensesRepository repository;
+
+    @Autowired
+    private ValueAddedTaxRateRepository vatRepository;
+
+    private SimpleDateFormat dateFormat;
+
+    @Before
+    public void setUp() {
+        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    }
+
+    @Test
+    @DisplayName("Should create a new expense and add it to the list of all expenses")
+    public void shouldCreateExpenseAndReturnAnUpdatedExpensesList() throws Exception {
+        ValueAddedTaxRate current = vatRepository.findEnabled();
+
+        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = dateFormat.parse("02/02/2018");
+        Expense expense = new Expense(date, 204.50, "Accomodation");
+        expense.setRate(current);
+
+        manager.persist(expense);
+        manager.flush();
+
+        List<Expense> expenses = repository.getAllExpenses();
+        assertTrue("Accomodation".equals(expenses.get(0).getReason()));
+    }
+}
---
+++ solution/expenses/src/test/java/com/engagetech/expenses/repository/ValueAddedTaxRateRepositoryTests.java
@@ -0,0 +1,55 @@
+package com.engagetech.expenses.repository;
+
+import com.engagetech.expenses.model.ValueAddedTaxRate;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+/**
+ * ValueAddedTaxRate Repository Unit Tests
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+public class ValueAddedTaxRateRepositoryTests {
+
+    @Autowired
+    private TestEntityManager manager;
+
+    @Autowired
+    private ValueAddedTaxRateRepository repository;
+
+    @Test
+    @DisplayName("Should add a VAT rate with field enabled set to FALSE")
+    public void shouldCreateVATRate() {
+        ValueAddedTaxRate rate = new ValueAddedTaxRate(4.4, false);
+        manager.persist(rate);
+        manager.flush();
+
+        ValueAddedTaxRate entity = repository.findEnabled();
+        assertTrue(entity != null);
+        assertTrue(entity.getEnabled());
+        assertNotEquals(4.4, entity.getRate());
+    }
+
+    @Test
+    @DisplayName("Should retrieve only one enabled VAT rate")
+    public void shouldReturnOneEnabledVATRate() {
+        // by default, there is one "enabled" entry in the vat_rate table
+        // this test should be able to retrieve that entity
+        ValueAddedTaxRate entity = repository.findEnabled();
+        assertTrue(entity != null);
+        assertTrue(entity.getEnabled());
+        assertNotNull(entity.getRate());
+    }
+}
