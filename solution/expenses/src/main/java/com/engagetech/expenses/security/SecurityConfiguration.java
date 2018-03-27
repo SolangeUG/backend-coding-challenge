9f5dba5c4c3d13fc7aabf7b922391703a1f0c75c
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-27 13:40:55 +0200

    Integrate a CORS filter with Spring Security

--- solution/expenses/src/main/java/com/engagetech/expenses/security/SecurityConfiguration.java
+++ solution/expenses/src/main/java/com/engagetech/expenses/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.engagetech.expenses.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 
 /**
@@ -41,16 +48,31 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors().configurationSource(corsConfigurationSource())
+                    .and()
                 .authorizeRequests()
-                .antMatchers("/static/assets/**").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
+                    .antMatchers("/static/assets/**").permitAll()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
                 .httpBasic();
 
         // To enable generation of csrf token:
         //  - remove .csrf().disable() call on http
         //  - and uncomment the following line
         //http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
