edd659e46d7072e7b17a8474da06fd7a79465e0e
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-29 15:55:48 +0200

    Temporary disable CSRF

--- solution/expenses/src/main/java/com/engagetech/expenses/security/SecurityConfiguration.java
+++ solution/expenses/src/main/java/com/engagetech/expenses/security/SecurityConfiguration.java
@@ -3,12 +3,14 @@ package com.engagetech.expenses.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -33,6 +35,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Value("${spring.security.user.roles}")
     private String role;
 
+    @Value("${server.allowedOrigins}")
+    private String allowedOrigins;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
@@ -46,10 +51,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
                 .csrf().disable()
                 .cors().configurationSource(corsConfigurationSource())
-                    .and()
+                .and()
                 .authorizeRequests()
                     .antMatchers("/static/assets/**").permitAll()
                     .anyRequest()
@@ -57,22 +63,36 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .and()
                 .httpBasic();
 
-        // To enable CSRF protection, and generation of a CSRF token:
-        //  - remove .csrf().disable() call on http
-        //  - and uncomment the following line
-        //http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        /* TODO : fix this! Enable CORS and CSRF protection!
+        http
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .and()
+                .cors().configurationSource(corsConfigurationSource())
+            .and()
+                .authorizeRequests()
+                .antMatchers("/static/assets/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                .anyRequest()
+                .authenticated()
+            .and()
+                .httpBasic();
+        */
+
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    protected CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
+
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+
         return source;
     }
 }
